d86a35cc281ad6cdafc23f30afb7412f6d8b9088
artemmz_ <artem825@mail.ru>
2015-04-03 20:28:25 +0300

    enhanced speed

--- src/FixFloor.java
+++ src/FixFloor.java
@@ -6,13 +6,17 @@ import java.util.*;
 */
 public class FixFloor{
 	
-	static final int[] leftRight 	= 	{ 1, 0, 0, -1 };
-	static final int[] upDown 		= 	{ 0, 1, -1, 0 };
+	static final int[] leftRight 	= 	{ -1, 0, 1, 0 };
+	static final int[] upDown 		= 	{ 0, 1, 0, -1 };
 	private static List<String> floorList = new ArrayList<String>();
-	private static List<List<Integer>> brokenGraph = new ArrayList<List<Integer>>();
+	private static List<List<Integer>> brokenBiGraph = new ArrayList<List<Integer>>();
+	private static boolean[] usedRand; // random matching to enhance performance
 	private static int[] matching;
-	private static int n;
-	private static int m;
+	private static int n; 
+	private static int m; 
+	private static int black = 0; // number of 1st set vertices
+	private static int white = 0; // number of 2nd set vertices
+	private static Map<Integer,Integer> blWhNumbers = new HashMap();
 	
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
@@ -28,7 +32,6 @@ public class FixFloor{
 		int brokenNumb = 0;
 		for (int i = 0; i < n; i++){
 			for (int j = 0; j < m; j++){
-				brokenGraph.add(new ArrayList<Integer>());
 				if ((i+j)%2 != 0 && isBroken(i,j)){ 
 					for (int k = 0; k < leftRight.length; k++){
 						int iNear = i + leftRight[k];
@@ -53,14 +56,27 @@ public class FixFloor{
 	}
 	
 	private static int maxMatching (){
-		matching = new int[n * m];
+		matching = new int[white];
 		Arrays.fill(matching, -1);
-		for (int v = 0; v < n * m; v++){
-			findPath(v, new boolean[n*m]);
+		usedRand = new boolean[black];
+		for (int i = 0; i < black; i++){
+			for (int j = 0; j < brokenBiGraph.get(i).size(); j++){
+				int current = brokenBiGraph.get(i).get(j);
+				if (matching[current] == -1){
+					matching[current] = i;
+					usedRand[i] = true;
+					break;
+				}
+			}
+		}
+		
+		for (int v = 0; v < black; v++){
+			if (usedRand[v]) continue;
+			findPath(v, new boolean[black]);
 		}
 		
 		int pairs = 0;
-		for (int to = 0; to < n * m; to++){
+		for (int to = 0; to < white; to++){
 			if (matching[to] != -1){
 				pairs++;
 			}
@@ -71,7 +87,7 @@ public class FixFloor{
 	private static boolean findPath(int v, boolean[] used){
 		if (used[v]) return false;
 		used[v] = true;
-		for (Integer to : brokenGraph.get(v)) {
+		for (Integer to : brokenBiGraph.get(v)) {
 			if (matching[to] == -1 || findPath(matching[to], used)) {
 				matching[to] = v;
 				return true;
@@ -89,6 +105,15 @@ public class FixFloor{
 	}
 	
 	private static void addEdge(int v, int to){
-		brokenGraph.get(v).add(to);
+		if (blWhNumbers.get(v) == null){
+			brokenBiGraph.add(new ArrayList<Integer>());
+			blWhNumbers.put(v,black);
+			black++;
+		}
+		if (blWhNumbers.get(to) == null){
+			blWhNumbers.put(to,white);
+			white++;
+		}
+		brokenBiGraph.get(blWhNumbers.get(v)).add(blWhNumbers.get(to));
 	}
 }
